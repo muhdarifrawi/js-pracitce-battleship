2133581da2e8656a71137042b094ff394affd210
Arif Rawi <muhdarifrawi@gmail.com>
2020-01-28 16:34:03 +0800

    initial commit.

---
+++ battleship.js
@@ -0,0 +1,8 @@
+// let's create some rules
+//first we need some ships
+// carrier - five spaces
+//battleship - four spaces
+//cruiser - three spaces
+//submarine - three spaces 
+//destroyer - two spaces
+
