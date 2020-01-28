eda253da43adffa695ff193ea84912ebedb0c41c
Arif Rawi <muhdarifrawi@gmail.com>
2020-01-28 17:23:48 +0800

    created object to hold information and placement of pieces.

--- battleship.js
+++ battleship.js
@@ -6,3 +6,13 @@
 //submarine - three spaces 
 //destroyer - two spaces
 
+//let's setup objects for them
+//so the string on index zero helps player recall how many spaces a piece holds
+let playablePieces = {
+    carrier : ["five spaces","B9","C9","D9","E9","F9"],
+    battleship : ["four spaces","G4","G5","G6","G7"],
+    cruiser : ["three spaces","C5","C6","C7"],
+    submarine : ["three spaces","F4","F5","F6"],
+    destroyer : ["two spaces","D2","E2"]
+}
+
